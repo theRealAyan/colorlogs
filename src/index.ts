c12d6d49aac57898105edc074c48936b13320c01
theRealAyan <ayanispro420@gmail.com>
2021-01-23 21:11:58 +0600

    Use dayjs

--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,9 @@ import {
 	dim
 } from 'colorette';
 
-function len(args: number) {
-	let str: string = String(args);
-	switch(str.length){
-		case 0:
-			str = '00' + str;
-			break;
-		case 1:
-			str = '0' + str;
-			break;
-	};
+import * as dayjs from 'dayjs';
 
-	return str;
-};
-
-function getTime(): string {
-  const date = new Date();
-
-    let h: string = String(date.getHours());
-
-	if(h.length < 1) h = '0' + h;
-    
-    const m: string = len(date.getMinutes());
-    const s: string = len(date.getSeconds());
-    const ms: string = len(date.getMilliseconds());
-
-    
-
-  return `${h}:${m}:${s}:${ms}`;
-
-};
+function getTime(): string {return dayjs().format('HH:mm:ss:SSS'); };
 
 export function info(text:string):void {
 	return console.log(`${gray(getTime())} ${bold(blueBright('INFO'))}    ${dim(text)}`);
