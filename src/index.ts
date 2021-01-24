560b79f5880af35820fe405e56cbb7e88a08f15d
theRealAyan <ayanispro420@gmail.com>
2021-01-24 08:42:17 +0600

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,14 @@ import * as dayjs from 'dayjs';
 function getTime(): string {return dayjs().format('HH:mm:ss:SSS'); };
 
 export function info(text:string):void {
-	return console.log(`${gray(getTime())} ${bold(blueBright('INFO'))}    ${dim(text)}`);
+	return console.log(`${gray(getTime())} ${bold(blueBright('INFO'))}    ${text}`);
 };
 export function success(text:string):void {
-    return console.log(`${gray(getTime())} ${bold(greenBright('SUCCESS'))} ${dim(text)}`);
+    return console.log(`${gray(getTime())} ${bold(greenBright('SUCCESS'))} ${text}`);
 };
 export function warn(text:string):void {
-    return console.log(`${gray(getTime())} ${bold(yellowBright('WARNING'))} ${dim(text)}`);
+    return console.log(`${gray(getTime())} ${bold(yellowBright('WARNING'))} ${text}`);
 };
 export function error(text:string):void {
-    return console.log(`${gray(getTime())} ${bold(redBright('ERROR'))}   ${dim(text)}`);
+    return console.log(`${gray(getTime())} ${bold(redBright('ERROR'))}   ${text}`);
 };
